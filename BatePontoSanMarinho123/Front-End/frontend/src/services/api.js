62b230b0bfc7cb0c77d50a97dfe34b2c8d3923b6
Deniel Elian Kasperowicz <132719249+Denielkas@users.noreply.github.com>
2026-03-22 17:24:55 -0300

    v4

--- BatePontoSanMarinho123/Front-End/frontend/src/services/api.js
+++ BatePontoSanMarinho123/Front-End/frontend/src/services/api.js
@@ -3,9 +3,11 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "/api",
   timeout: 15000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// ADD ESTA PARTE:
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
